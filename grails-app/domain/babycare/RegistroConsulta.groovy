61dbf2c5bd3f7d3c36dc0a97d9466ff358504f7b
Seval931 <svalencias@unal.edu.co>
2015-04-15 19:18:57 -0500

    base tutor, registroDeConsulta

--- grails-app/domain/babycare/RegistroConsulta.groovy
+++ grails-app/domain/babycare/RegistroConsulta.groovy
@@ -1,7 +1,15 @@
 package babycare
 
 class RegistroConsulta {
-
+	
+	String fecha   //en caso de que sea mejor otro tipo de dato puede ser cambiado
+	String motivoConsulta 
+	String conceptoMedico
+	
     static constraints = {
+		belongsTo=[historiaMedica:HistoriaMedica]		//herencia
+		fecha nullable:false							//esta se deberia auto-asignar con la fecha del momento de diligenciamiento
+		motivoConsulta blank:false, min:5, max:70		//los 70 caracteres deberian garantizar ser concisos 
+		conceptoMedico blank:false, min:10				//no tiene un maximo porque un concepto medico no se puede limitar
     }
 }
--- grails-app/domain/babycare/Tutor.groovy
+++ grails-app/domain/babycare/Tutor.groovy
@@ -1,7 +1,12 @@
 package babycare
 
 class Tutor extends Persona {
-
+	
+	int registroEPS
+	Paciente hijos
+	
     static constraints = {
+		registroEPS blank:false, nullable:false			//en caso de que se este ingresando un usuario no puede ser blank, null por si algo
+		hasMany=[paciente:Paciente]						//un tutor puede tener varios hijos
     }
 }
