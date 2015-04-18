8336c3e10f181829b75d7babce693838b84a2231
LorenaGS <dlgomezs@unal.edu.co>
2015-04-17 23:07:41 -0500

    - Diseño de clase de dominio HistoriaClinica con atributos y restricciones,generada vista y controlador.

--- grails-app/domain/babycare/HistoriaClinica.groovy
+++ grails-app/domain/babycare/HistoriaClinica.groovy
@@ -1,7 +1,16 @@
 package babycare
 
 class HistoriaClinica {
+	
+	String codigoPlanilla
+	Date fechaHistoria
+	String lugarHistoria
+	int numeroHistoria
 
     static constraints = {
+		codigoPlanilla (blank: false, nullable: false, matches: "^(?=.*[A-Z])(?=.*[0-9])([A-Z0-9]+)");
+		fechaHistoria (blank: false, nullable: false);
+		lugarHistoria (blank: false);
+		numeroHistoria (blank: false, nullable:false, minsize: 7..10);
     }
 }
